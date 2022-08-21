044d209b630fded548004be682a0ae715ba5f6d7
nandatritona <nandhatritona@gmail.com>
2022-08-21 20:05:38 +0700

    setup dotenv

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hello world');
+  res.send(process.env.APP_NAME);
 });
 
 router.get("/me", (req, res) => {
